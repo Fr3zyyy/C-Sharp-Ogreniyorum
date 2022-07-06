f176a8b12b6c4e73f82aaea3c1094673a8f1fb08
Fr3zyy <veliaktasdc@gmail.com>
2022-07-06 10:54:58 +0300

    Dolardan Tl Ye Çevirme ve Eurodan Tl ye Çevirme Yapıldı

---
+++ C Sharp Ogreniyorum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Ogreniyorum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal dolar = 17.1734m;
+            decimal euro = 17.62155m;
+
+            Console.Write("Dolar'dan Tl Ye Çevir : ");
+            int dolartl = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Euro'dan Tl Ye Çevir : ");
+
+            int eurotl = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dolar => Tl " + dolar * dolartl);
+            Console.WriteLine("Euro => Tl " + euro * eurotl);
+
+            Console.Read();
+        }
+    }
+}
